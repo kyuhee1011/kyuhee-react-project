0c2cda1c2cd4760f4b648184f4d158b2659c3d0e
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-06-20 16:55:18 -0400

    Adds react-router-dom and food components

---
+++ src/components/Food.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+function Food() {
+  const [desserts, setDessert] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/food")
+      .then((r) => r.json())
+      .then((desserts) => {
+        console.log("Sweet Dessert pictures loaded");
+        setDessert(desserts);
+      });
+  }, []);
+
+  return <div>{desserts.map((dessert) => dessert.name)}</div>;
+}
+export default Food;
