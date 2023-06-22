5e6fa51e07c0b3e04809bf9158e319c233269be5
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-06-22 05:48:23 -0400

    Revise DessertContainer component

--- src/components/DessertContainer.js
+++ src/components/DessertContainer.js
@@ -5,17 +5,47 @@ import { useState, useEffect } from "react";
 //Dessert Page
 
 function DessertContainer() {
-  const [desserts, setDessert] = useState([]);
+  const [desserts, setDesserts] = useState([]);
+  const [favorites, setFavorites] = useState(false);
+  const [searchSweet, setSearchSweet] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:3000/desserts")
       .then((r) => r.json())
       .then((desserts) => {
-        console.log("Sweet Dessert pictures loaded");
-        setDessert(desserts);
+        console.log("Sweety sugar Dessert loaded");
+        setDesserts(desserts);
       });
   }, []);
-  return <div>{desserts.map((dessert) => dessert.name)}</div>;
+
+  const dessertsFavList = () => {};
+
+  const handleMyFavDessert = () => {
+    setFavorites((favorites) => !favorites);
+  };
+
+  const onMyFavTask = (myFavorite) => {
+    const addMyFav = desserts.map((desserts) =>
+      desserts.id === myFavorite.id ? myFavorite : desserts
+    );
+    setFavorites(addMyFav);
+  };
+
+  //   const itemsToDisplay = items.filter((item) => {
+  //     if (selectedCategory === "All") return true;
+
+  //     return item.category === selectedCategory;
+  //   });
+
+  return (
+    <DessertList
+      desserts={dessertsFavList}
+      searchSweet={searchSweet}
+      favorites={favorites}
+      onMyFavorClick={handleMyFavDessert}
+      onFavoriteDessert={onMyFavTask}
+    />
+  );
 }
 
 export default DessertContainer;
